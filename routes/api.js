809f1c0e1fcd0bd8215a9995c18a093674fd9539
alexP-bu <apozin@bu.edu>
2022-06-26 13:13:48 -0400

    cleaned up code a lot

--- routes/api.js
+++ routes/api.js
@@ -4,36 +4,18 @@ const axios = require('axios');
 const API = require('../config/dogAPI');
 
 /* GET users listing. */
-router.get('/dogs/', function(req, res) {
-     //get dog search data
-     searchDogs(req.query.search)
-         .then( async response => {
-            //for each dog, if it has an image reference id, map the url to the dog
-            let dogs = await response.data
-                .map( async (dog) => {
-                    if (dog.reference_image_id != null){
-                        dog.imageURL = await getImageURL(dog.reference_image_id);
-                    }
-                return dog;
-            });
-
-        Promise.all(dogs).then(result => res.send(result));
-     });
+router.get('/dogs/', async (req, res) => {
+    const query = req.query.search;
+    const dogsPromise = (await axios.get(API.url, { params: { q: query, x_api_key: API.key }})).data
+        //if a dog has an image, map it
+        .map(async (dog) => {
+            let id = dog.reference_image_id;
+            if (id != null){
+                dog.imageURL = (await axios.get('https://api.thedogapi.com/v1/images/' + id, { params: { x_api_key: API.key }})).data.url;
+            }
+            return dog;
+        });
+    Promise.all(dogsPromise).then(dogs => res.send(dogs));
 });
-
-const getImageURL = async (id) => {
-    let response = await axios.get('https://api.thedogapi.com/v1/images/' + id, { params: { x_api_key: API.key }})
-        .catch(err => {
-            console.log(err);
-        });
-    return response.data.url;
-}
-
-const searchDogs = async (query) => {
-    return await axios.get(API.url, { params: { q: query, x_api_key: API.key }})
-        .catch(error => {
-            console.error(error);
-        });
-}
 
 module.exports = router;
