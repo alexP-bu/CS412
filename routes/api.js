847d5ae135a005e8cecdb9075d36697a556f41c4
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2022-06-26 23:41:41 -0400

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,25 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const API = require('../config/dogAPI');
+const redis = require('redis');
+
+const redisClient = redis.createClient();
 
 /* GET users listing. */
 router.get('/dogs/', async (req, res) => {
     const query = req.query.search;
-    const dogsPromise = (await axios.get(API.url, { params: { q: query, x_api_key: API.key }})).data
-        //if a dog has an image, map it
-        .map(async (dog) => {
+
+    if(redisClient.exists(query)){
+        //get objects
+    }else{
+
+    }
+
+    const dogsPromise =
+        (await axios.get(API.url, { params: { q: query, x_api_key: API.key }}))
+        //if a dog has a reference image, map it
+        .data
+        .map( async (dog) => {
             let id = dog.reference_image_id;
             if (id != null){
                 dog.imageURL = (await axios.get('https://api.thedogapi.com/v1/images/' + id, { params: { x_api_key: API.key }})).data.url;
