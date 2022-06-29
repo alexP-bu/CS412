c1f5f4601eabf644778c274ad24715c4d1099fa3
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2022-06-29 12:18:01 -0400

    added redis for caching

--- routes/api.js
+++ routes/api.js
@@ -10,24 +10,36 @@ const redisClient = redis.createClient();
 router.get('/dogs/', async (req, res) => {
     const query = req.query.search;
 
-    if(redisClient.exists(query)){
-        //get objects
-    }else{
-
-    }
-
-    const dogsPromise =
-        (await axios.get(API.url, { params: { q: query, x_api_key: API.key }}))
-        //if a dog has a reference image, map it
-        .data
-        .map( async (dog) => {
-            let id = dog.reference_image_id;
-            if (id != null){
-                dog.imageURL = (await axios.get('https://api.thedogapi.com/v1/images/' + id, { params: { x_api_key: API.key }})).data.url;
-            }
-            return dog;
+    //if query was searched before, return dogs list associated with it
+    if(await redisClient.hExists('dogs', query)){
+        //get dogs and send result
+        await redisClient.HGET('dogs', query, (err, dogs) => {
+            res.send(JSON.parse(dogs))
         });
-    Promise.all(dogsPromise).then(dogs => res.send(dogs));
+    }else{
+        //object doesn't exist in redis, call the API and store the result associated with the search
+        const dogsPromise =
+            (await axios.get(API.url, { params: { q: query, x_api_key: API.key }}))
+                //if a dog has a reference image, map it
+                .data
+                .map( async (dog) => {
+                    let id = dog.reference_image_id;
+                    if (id != null){
+                        dog.imageURL = (await axios.get('https://api.thedogapi.com/v1/images/' + id, { params: { x_api_key: API.key }})).data.url;
+                    }
+                    return dog;
+                });
+
+        //resolve promises
+        //store result in redis with key = query and the dog object as a json string
+        Promise.all(dogsPromise).then(dogs => {
+            redisClient.HSET('dogs', query, JSON.stringify(dogs));
+            redisClient.HGET('dogs', query, (err, dogs) => {
+                console.log(JSON.parse(dogs));
+            });
+            res.send(dogs);
+        });
+    }
 });
 
 module.exports = router;
